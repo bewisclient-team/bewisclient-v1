588a2f652d7a38705a927ad732fbc31f0042b315
Bewis09 <99420254+Bewis09@users.noreply.github.com>
2023-08-17 17:53:19 +0200

    Delete src/main/java/bewis09 directory

--- src/main/java/bewis09/util/InGameHudImplementer.java
+++
@@ -1,7 +0,0 @@
-package bewis09.util;
-
-import net.minecraft.client.util.math.MatrixStack;
-
-public interface InGameHudImplementer {
-    void renderWidgets(MatrixStack matrices, boolean bl);
-}
--- src/main/java/bewis09/util/OptionWidget.java
+++
@@ -1,9 +0,0 @@
-package bewis09.util;
-
-public interface OptionWidget {
-    int getXOffset();
-    int getYOffset();
-    void setX(int x);
-    void setY(int y);
-    void setVisible(boolean visible);
-}
